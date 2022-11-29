1107a16c7545c610ba5c57c6ecbbea4f79be3981
mauro sala <mauro.sala@gmail.com>
2022-11-29 16:30:05 +0100

    fix

--- controllers/all.controller.js
+++ controllers/all.controller.js
@@ -9,31 +9,62 @@ router.all('/*', async (req, res, next) => {
     const svc = req.baseUrl.split('/')[1]
     const params = req.baseUrl.split('/').slice(2).join('/')
 
+    if (req.baseUrl === '/auth/user') {
+      // return res.status(403).json({ message: 'unauthenticated' })
+      return res.status(200).json({
+        id: 'krateo',
+        displayName: 'krateo',
+        username: 'krateo',
+        provider: 'basic',
+        email: 'krateo@krateo.io'
+      })
+    }
+
     switch (svc) {
+      case 'package':
+        url = `http://localhost:8091`
+        break
       case 'auth':
+        // url = 'https://api.krateo.site/auth/'
         url = 'http://localhost:8093/'
         break
       case 'deployment':
         url = 'http://localhost:8085/'
         break
       case 'pipeline':
-        // url = 'http://localhost:8085/'
+        url = 'http://localhost:8090/'
         break
       case 'codequality':
         // url = 'http://localhost:8085/'
         break
       case 'doc':
-        // url = 'http://localhost:8085/'
+        url = 'http://localhost:8089/'
         break
       case 'capi':
         url = 'http://localhost:8091/'
+        break
+      case 'notification':
+        url = 'http://localhost:4000/'
+        break
+      case 'secret':
+        url = 'http://localhost:8095/'
+        break
+      case 'template':
+        // url = 'https://api.krateo.site/template/'
+        url = 'http://localhost:8086/'
+        break
+      case 'component':
+        url = 'http://localhost:8092/'
+        break
+      case 'event':
+        url = 'https://api.krateo.site/event/'
         break
       default:
         url = null
     }
 
     if (!url) {
-      return res.status(500).send()
+      return res.status(505).send()
     }
 
     url += params
@@ -55,7 +86,9 @@ router.all('/*', async (req, res, next) => {
         res.status(response.status).json(response.data)
       })
       .catch((error) => {
-        res.status(500).json(error)
+        res
+          .status(error.response.status || 500)
+          .json({ message: error.response?.data?.message || error.message })
       })
   } catch (err) {
     next(err)
